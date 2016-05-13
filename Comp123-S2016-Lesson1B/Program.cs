f022dfea1d3c7f0a4e1a846d682b8d53ef9a2147
ryan-dantow <rdantow@gmail.com>
2016-05-13 10:43:56 -0400

    Initial Commit

---
+++ Comp123-S2016-Lesson1B/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Author: Ryan Dantow
+ * Date: May 13th 2016
+ * Studnet ID: 300775845
+ * 
+ * Description: this program demonstrates GitHub and version control
+ * best practices. We also explore unit testing
+ * 
+ * Version 0.1 - Initial Version
+ */
+namespace Comp123_S2016_Lesson1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
